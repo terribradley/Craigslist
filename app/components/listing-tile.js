b8e2d0508117c68543102cd2308574273c463c26
Alvin Ng and Terri Bradley <alvin+terri@>
2016-08-22 15:50:37 -0700

    sort listings by date

--- app/components/listing-tile.js
+++ app/components/listing-tile.js
@@ -2,5 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   sortBy: ['timestamp:asc'],
-  sortedListings: Ember.computed.sort('category.listings', 'sortBy')
+  sortedListings: Ember.computed.sort('category.listings', 'sortBy'),
+  actions: {
+    update(listing, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          listing.set(key,params[key]);
+        }
+      });
+      listing.save();
+      this.transitionTo('category', params.category);
+    },
+    destroyListing(listing, params) {
+      listing.destroyRecord();
+      this.transitionTo('category', params.category);
+    }
+  }
 });
--- app/routes/category.js
+++ app/routes/category.js
@@ -13,19 +13,6 @@ export default Ember.Route.extend({
        return category.save();
      });
      this.transitionTo('category', params.category);
-    },
-    update(listing, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          listing.set(key,params[key]);
-        }
-      });
-      listing.save();
-      this.transitionTo('category', params.category);
-    },
-    destroyListing(listing, params) {
-      listing.destroyRecord();
-      this.transitionTo('category', params.category);
-    }
+    }  
    }
 });
